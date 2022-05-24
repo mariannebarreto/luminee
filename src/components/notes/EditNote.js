90b7d2a6b189736bccdd2a81e40e116f8367e7df
mariannebarreto <barretoink@icloud.com>
2022-05-24 11:08:48 -0500

    Edit: corregir error custom object

--- src/components/notes/EditNote.js
+++ src/components/notes/EditNote.js
@@ -1,16 +1,57 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { BsFileEarmarkCheckFill } from 'react-icons/bs';
 import { IoIosCloseCircle } from 'react-icons/io';
 import { MdHome } from 'react-icons/md';
 import './EditNote.css';
+import {
+  setDoc, collection, getDoc, doc, serverTimestamp,
+} from 'firebase/firestore';
+import { db, auth } from '../../lib/firebase-config';
 
 function EditNote() {
+  // NAVEGA
   const navigate = useNavigate();
+  const { id } = useParams();
+
+  const noteToEdit = {
+    title: '',
+    note: '',
+    userID: auth.currentUser,
+    date: serverTimestamp(),
+  };
+
+  const [updateNote, setupdateNote] = useState(noteToEdit);
+
+  const getNotesId = async () => {
+    try {
+      const collectionRef = collection(db, 'notes');
+      const docSnap = await getDoc(collectionRef, id);
+      setupdateNote(docSnap.data());
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  // -------HANDLERS
 
   const handleNavHome = () => {
     navigate('/Home');
   };
+
+  const handleEditNote = async (e) => {
+    e.preventDefault();
+    getNotesId();
+    const docRef = doc(db, 'notes', id);
+    await setDoc(docRef, updateNote, {
+      email: auth.currentUser.email,
+      date: serverTimestamp(),
+    }, { merge: true }).then(navigate('/Home'));
+  };
+
+  useEffect(() => {
+    getNotesId();
+  }, []);
 
   return (
   /* <div>
@@ -25,14 +66,29 @@ function EditNote() {
           {' '}
         </header>
       </div>
-      <form className="writeAreaContainer">
-        <input type="text" id="titleNote" placeholder="Title" />
-        <textarea id="bodyNote" placeholder="Write your note here" rows="40" cols="40" />
+      <form className="writeAreaContainer" onSubmit={(e) => { handleEditNote(e); }}>
+        <input
+          type="text"
+          id="titleNote"
+          placeholder="Title"
+          value={updateNote.title}
+          onChange={(e) => { setupdateNote({ ...updateNote, title: e.target.value }); }}
+        />
+        <textarea
+          id="bodyNote"
+          placeholder="Write your note here"
+          rows="40"
+          cols="40"
+          value={updateNote.note}
+          onChange={(e) => { setupdateNote({ ...updateNote, note: e.target.value }); }}
+        />
+
         <footer className="footerMenu">
           <BsFileEarmarkCheckFill
             type="submit"
             className="iconNote"
             size="2.5em"
+            onSubmit={(e) => { handleEditNote(e); }}
           />
           <MdHome type="submit" className="iconNote" size="3em" onClick={handleNavHome} />
           <IoIosCloseCircle
@@ -43,6 +99,7 @@ function EditNote() {
           />
         </footer>
       </form>
+
     </section>
   );
 }
